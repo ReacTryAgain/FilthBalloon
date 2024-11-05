f8a5347ad11273c4cda2dcf27820a3174f2b4f95
John Doe <ok63477@gmail.com>
2024-11-05 16:04:08 +0900

    메인페이지 css 변경

--- src/components/mainPage/MainPage.styles.js
+++ src/components/mainPage/MainPage.styles.js
@@ -15,7 +15,7 @@ const styles = {
   },
   content: {
     display: "flex",
-    width: "90%",
+    width: "95%",
     justifyContent: "space-between",
   },
   sidebar: {
@@ -36,7 +36,7 @@ const styles = {
     flexDirection: "column",
     alignItems: "flex-start", // 전체를 왼쪽 정렬
     gap: "15px",
-    width: "300px",
+    width: "250px",
   },
   formContainer: {
     display: "flex",
